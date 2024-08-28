5ed8097b794fb224eb928e2ec9316b6cab2d1242
GabrielRomo33 <rromo@tiendas-grand.com.mx>
2024-08-28 09:20:24 -0600

    Mas

--- 06-shopping-cart/src/components/Cart.jsx
+++ 06-shopping-cart/src/components/Cart.jsx
@@ -1,10 +1,30 @@
 import React, { useId } from 'react'
 import { CartIcon, ClearCartIcon } from './icons';
 import './Cart.css'
+import { useCart } from '../hooks/useCart';
+// import { Products } from './Products';
 
+
+function CartItem ({image, price, title, quantity, addToCart}) {
+    return(
+        <li>
+            <img src={image} alt={title} />
+            <div>
+                <strong>{title}</strong> - ${price}
+            </div>
+            <footer>
+                <small>
+                    Qty: {quantity}
+                </small>
+                <button onClick={addToCart}>+</button>
+            </footer>
+        </li>
+    )
+}
 
 export const Cart = () => {
     const cartCheckboxId = useId();
+    const { cart, clearCart, addToCart } = useCart();
   return (
     <>
         <label className='cart-button' htmlFor={cartCheckboxId}>
@@ -14,20 +34,17 @@ export const Cart = () => {
 
         <aside className='cart'>
             <ul>
-                <li>
-                    <img src="https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg" alt="Disk" />
-                    <div>
-                        <strong>Disk</strong>
-                    </div>
-                    <footer>
-                        <small>
-                            Qty: 1
-                        </small>
-                        <button>+</button>
-                    </footer>
-                </li>
+                {
+                    cart.map(product => (
+                        <CartItem
+                        key={product.id}
+                        addToCart={() => addToCart(product)}
+                        {...product}
+                        />
+                    ))
+                }
             </ul>
-            <button>
+            <button onClick={clearCart}>
                 <ClearCartIcon />
             </button>
         </aside>
--- 06-shopping-cart/src/components/Footer.jsx
+++ 06-shopping-cart/src/components/Footer.jsx
@@ -8,11 +8,11 @@ export const Footer = () => {
 
   return (
     <footer className='footer'>
-        {/* <h4>Prueba Tecnica con React ⚛️ - <span>@RenatoRomo</span></h4>
-        <h5>Shopping Cart con useContext & useReducer</h5> */}
-        {
+        <h4>Prueba Tecnica con React ⚛️ - <span>@RenatoRomo</span></h4>
+        <h5>Shopping Cart con useContext & useReducer</h5>
+        {/* {
             JSON.stringify(cart, null, 2)
-        }
+        } */}
         {/* <h5>Category: {filters.category}</h5> <h5>Min Price: {filters.minPrice}</h5> */}
     </footer>
   )
